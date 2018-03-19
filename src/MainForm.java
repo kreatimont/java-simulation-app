a968b6f7336b460639f6cb23b2c4e077d4bb702f
kreatimont <nadtoka.alexandr@gmail.com>
2018-03-19 19:39:29 +0200

    added diagram panel, rename project

--- src/MainForm.java
+++ src/MainForm.java
@@ -1,33 +1,41 @@
 import rnd.Negexp;
 import widgets.ChooseRandom;
+import widgets.Diagram;
 
 import javax.swing.*;
+import java.awt.*;
+
+//время сборки зерна
+//время разгрузки
+//врея загрузки
 
 public class MainForm {
 
     private JPanel modelConfigurationPanel;
-    private ChooseRandom timeModeling;
     private JSpinner carAmount;
     private JSpinner combineAmount;
+    private ChooseRandom timeModeling;
     private ChooseRandom timeAssembly;
     private ChooseRandom timeLoadingCar;
     private ChooseRandom timeUnloadingCar;
     private JButton runBtn;
+    private JTabbedPane tabbedPane;
+    private JPanel diagramTabPanel;
+    private JTextArea resultTextArea;
+    private Diagram resultDiagram;
 
-    //время сборки зерна
-    //время разгрузки
-    //врея загрузки
     private MainForm() {
         carAmount.setValue(5);
         combineAmount.setValue(2);
         timeModeling.setRandom(new Negexp(1));
     }
-//
+
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
+        JFrame frame = new JFrame("Java Simulation RGR");
         frame.setContentPane(new MainForm().modelConfigurationPanel);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.pack();
+        frame.setMinimumSize(new Dimension(800, 500));
         frame.setVisible(true);
     }
 
